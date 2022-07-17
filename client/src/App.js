e67e465b01c3b524d71d7705ddcce752c56521e4
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-17 14:52:22 -0400

    import apollo app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost'
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+
 
 function App() {
   return (
